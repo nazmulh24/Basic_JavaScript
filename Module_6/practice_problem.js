b9e0838d9634cc432504872f516abebdd6244589
Nazmul Hossain <119653933+nazmulh24@users.noreply.github.com>
2025-05-09 21:35:14 +0600

    Update practice_problem.js

--- Module_6/practice_problem.js
+++ Module_6/practice_problem.js
@@ -3,7 +3,23 @@
 const number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 //--> Find the even_Number of all numbers in the array
 
-const even_number = number.filter((num) => num % 2 == 0);
+const even_Number = (array) => {
+  const even_number = [];
+
+  for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+
+    if (element % 2 == 0) {
+      even_number.push(element);
+    }
+  }
+
+  return even_number;
+};
+const even_number = even_Number(number);
+console.log(even_number);
+
+// const even_number = number.filter((num) => num % 2 == 0);
 // console.log(...even_number);
 
 // const oddNumbers = number.filter((num) => num % 2 !== 0);
